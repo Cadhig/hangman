f64837139855fab3590be344b3563b14fdd2095f
Cadence Higgins <cadencehiggins@gmail.com>
2024-07-11 22:45:36 -0500

    computer picks random veggie from array, print empty spaces for each letter in word

--- hangman.py
+++ hangman.py
@@ -6,3 +6,13 @@ words = ['artichoke', 'asparagus', 'aubergene', 'avocado', 'beet',
             'collards', 'jicama', 'kale', 'kohlrabi', 'leek', 'arugula', 'lettuce', 'mushroom',
             'okra', 'onion', 'parsnip', 'peas', 'pepper', 'potato', 'pumpkin', 'radish', 'rhubarb',
             'rutabaga', 'shallot', 'spinach', 'squash', 'tomatillo', 'tomato', 'turnip', 'zucchini']
+
+
+word = random.choice(words)
+
+if __name__ == '__main__':
+    print('Hangman! HINT: Vegetable')
+
+    for i in word:
+        print('_', end=" ")
+    print()

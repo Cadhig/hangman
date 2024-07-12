d3a9f63ae41ebed83a1e903628c4d40e350191c3
Cadence Higgins <cadencehiggins@gmail.com>
2024-07-11 22:38:14 -0500

    created words array

---
+++ hangman.py
@@ -0,0 +1,8 @@
+import random
+
+words = ['artichoke', 'asparagus', 'aubergene', 'avocado', 'beet',
+          'broccoli', 'broccolini', 'cabbage', 'carrot',
+            'cauliflower', 'celery', 'chard', 'corn', 'cucumber', 'daikon', 'eggplant', 'garlic',
+            'collards', 'jicama', 'kale', 'kohlrabi', 'leek', 'arugula', 'lettuce', 'mushroom',
+            'okra', 'onion', 'parsnip', 'peas', 'pepper', 'potato', 'pumpkin', 'radish', 'rhubarb',
+            'rutabaga', 'shallot', 'spinach', 'squash', 'tomatillo', 'tomato', 'turnip', 'zucchini']
